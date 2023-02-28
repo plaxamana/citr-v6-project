de3c50bf562e373359db330a4e62b08bc8c77c65
Philip Laxamana <philip.laxamana@hotmail.com>
2023-02-27 20:01:05 -0500

    latest

---
+++ 04-hooks/src/App.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Details from "./Details";
+import ThemeContext from "./ThemeContext";
+
+const App = () => {
+  const theme = useState("darkblue");
+
+  return (
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <Router>
+          <header>
+            <Link to="/">
+              <h1>Adopt Me!</h1>
+            </Link>
+          </header>
+          <Switch>
+            <Route path="/details/:id">
+              <Details />
+            </Route>
+            <Route path="/" exact>
+              <SearchParams />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
+  );
+};
+
+render(<App />, document.getElementById("root"));
